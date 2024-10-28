5c86e8480e5242274c81f0c343f585eda0a7279a
Sruthi Naluvala <sruthinaluvala55@gmail.com>
2024-10-28 16:23:43 +0530

    EXPRESS
    Express handles request from server. Used postman to send  a request and verify it.

---
+++ express/sample-server/server.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const Port=3000
+
+const app=express()
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.status(200).json("Hello");
+})
+app.listen(Port,()=>{
+    console.log("server is running in port:"+Port)
+    console.log("server is running in port:"+Port)
+})
